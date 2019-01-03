eea9d9c188c4625de220b74522f24187a40f9b53
Cassady <cassady801@gmail.com>
2019-01-03 00:40:37 -0700

    current working version

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
 var game = {
     human: "",
     computer: "",
-    currentTurn: "Player1" 
+    currentTurn: "" 
 };
 
-var squares = document.querySelectorAll(".square") 
+var displayMsg = document.querySelector("#display");
+var squares = document.querySelectorAll(".square") ;
 
 //* Recall to calculate indices in row-major matrix 
 //* we do index = X+Y * Width
 
 playGame();
-
-
 
 function switchTurn() {
     if (game.currentTurn === "Player1") {
@@ -21,25 +20,51 @@ function switchTurn() {
     }
 }
 
+function init() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].setAttribute("selected", "false");
+    }
+    game.currentTurn = "Player1";
+}
+
+
+
 function playGame() {
+    init();
+
     for (var i = 0; i < squares.length; i++) {       
         squares[i].addEventListener("click", function() {
-            if (this.getAttribute("disabled") === "false") {
+            if (this.getAttribute("selected") === "false") {
                 if (game.currentTurn === "Player1") {
                     var setX = document.createTextNode("X");
                     this.appendChild(setX);
-                    this.setAttribute("disabled", "true");  
+                    this.setAttribute("selected", "true");  
                     switchTurn(); 
                 } else {
                     var setO = document.createTextNode("O");
                     this.appendChild(setO);
-                    this.setAttribute("disabled", "true");   
+                    this.setAttribute("selected", "true");   
                     switchTurn();
                 }
-            } else {
-                alert("This square has been picked already!");
-                return;
             }
         });
     }
 }
+
+function checkWin() {
+    if (game.currentTurn === "Player1") {
+        checkWin1("X");
+    } else {
+        checkWin1("O");
+    }
+}
+
+function checkWin1(pSymbol) {
+    if (squares[0].textContent === pSymbol &&
+        squares[1].textContent === pSymbol &&
+        squares[2].textContent === pSymbol) {
+            displayMsg.textContent = "You won!";
+            return true;
+        }
+    return false;
+};
