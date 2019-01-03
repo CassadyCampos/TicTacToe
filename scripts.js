58efd59f1570abe0b107b372481eed0219204e0b
Cassady <cassady801@gmail.com>
2019-01-03 01:19:28 -0700

    Added all win possibilities.

--- scripts.js
+++ scripts.js
@@ -59,8 +59,22 @@ function reset() {
 function checkWin() {
     if (currentTurn === "Player1") {
         checkWin1("X");
+        checkWin2("X");
+        checkWin3("X");
+        checkWin4("X");
+        checkWin5("X");
+        checkWin6("X");
+        checkWin7("X");
+        checkWin8("X");
     } else {
         checkWin1("O");
+        checkWin2("O");
+        checkWin3("O");
+        checkWin4("O");
+        checkWin5("O");
+        checkWin6("O");
+        checkWin7("O");
+        checkWin8("O");
     }
 };
 
@@ -73,6 +87,86 @@ function checkWin1(pSymbol) {
         }
     return false;
 };
+
+function checkWin2(pSymbol) {
+    if (squares[3].textContent === pSymbol &&
+        squares[4].textContent === pSymbol &&
+        squares[5].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin3(pSymbol) {
+    if (squares[6].textContent === pSymbol &&
+        squares[7].textContent === pSymbol &&
+        squares[8].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin4(pSymbol) {
+    if (squares[0].textContent === pSymbol &&
+        squares[3].textContent === pSymbol &&
+        squares[6].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin5(pSymbol) {
+    if (squares[1].textContent === pSymbol &&
+        squares[4].textContent === pSymbol &&
+        squares[7].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin5(pSymbol) {
+    if (squares[1].textContent === pSymbol &&
+        squares[4].textContent === pSymbol &&
+        squares[7].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin6(pSymbol) {
+    if (squares[2].textContent === pSymbol &&
+        squares[5].textContent === pSymbol &&
+        squares[8].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin7(pSymbol) {
+    if (squares[0].textContent === pSymbol &&
+        squares[4].textContent === pSymbol &&
+        squares[8].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
+
+function checkWin8(pSymbol) {
+    if (squares[2].textContent === pSymbol &&
+        squares[4].textContent === pSymbol &&
+        squares[6].textContent === pSymbol) {
+            displayMsg.textContent = currentTurn + " has won!";
+            gameOver();
+        }
+    return false;
+}
 
 function gameOver() {
     for (var i = 0; i < squares.length; i++) {
