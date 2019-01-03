23b3cf791ead7eb2da2ae6041de9bb124e6cb77c
Cassady <cassady801@gmail.com>
2019-01-02 22:57:35 -0700

    Got X's and O's selections working

--- scripts.js
+++ scripts.js
@@ -1,41 +1,45 @@
-var _1Player = "Player1";
-var _2Player = "Player2";
-var playerTurn = _1Player;
-var playableSquares = [];
-var squares = document.querySelectorAll(".square")   
+var game = {
+    human: "",
+    computer: "",
+    currentTurn: "Player1" 
+};
+
+var squares = document.querySelectorAll(".square") 
 
 //* Recall to calculate indices in row-major matrix 
 //* we do index = X+Y * Width
-humanSelect();
-computerSelect();
+
+playGame();
 
 
-function turnSeq() {
 
-}
-
-
-function humanSelect() {
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("click", function() {
-            if (this.getAttribute("disabled") === "false") {
-                var textnode = document.createTextNode("X");
-                this.appendChild(textnode);
-                this.setAttribute("disabled", "true");
-            } else {
-                alert("This square has been picked already!");
-            }
-        });
+function switchTurn() {
+    if (game.currentTurn === "Player1") {
+        game.currentTurn = "Player2";
+    } else {
+        game.currentTurn = "Player1";
     }
 }
 
-function computerSelect() {
-    for ( var i = 0; i < squares.length; i++) {
-        if(squares[i].getAttribute("disabled") === "false") {
-            var textnode = document.createTextNode("O");
-            squares[i].appendChild(textnode);
-            squares[i].setAttribute("disabled", "true");
-            return;
-        }
+function playGame() {
+    for (var i = 0; i < squares.length; i++) {       
+        squares[i].addEventListener("click", function() {
+            if (this.getAttribute("disabled") === "false") {
+                if (game.currentTurn === "Player1") {
+                    var setX = document.createTextNode("X");
+                    this.appendChild(setX);
+                    this.setAttribute("disabled", "true");  
+                    switchTurn(); 
+                } else {
+                    var setO = document.createTextNode("O");
+                    this.appendChild(setO);
+                    this.setAttribute("disabled", "true");   
+                    switchTurn();
+                }
+            } else {
+                alert("This square has been picked already!");
+                return;
+            }
+        });
     }
 }
